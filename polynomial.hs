914d630df682809472ac89b5c767f5e81ccc33d4
ba205 <ba205@users.noreply.github.com>
2015-06-30 16:01:52 -0400

    Update polynomial.hs
    Much better. Only real problem is Show function is a little ugly, and if the constant term is 0, it will always show up. Besides that, it's looking good, and documentation is better.

--- polynomial.hs
+++ polynomial.hs
@@ -3,65 +3,89 @@ module HW04 where
 import Data.List
 newtype Poly a = P [a]
 
--- Exercise 1 -----------------------------------------
+{- A polynomial typeclass, represented as lists.
+   The first element is the constant term, the 
+   second element is the linear term, and so on. -}
 
--- x is a polynomial of degree 1, so it should be represented as 1x + 0
+{- x is a polynomial of degree 1, so it should be represented as 1x + 0 -}
 x :: Num a => Poly a
 x = P [0,1]
 
--- Exercise 2 ----------------------------------------
-
--- Checking for polynomial equality
--- Trivial if they do not have the same number of terms
+{- Allows checking for polynomial equality.
+   Trivial if they do not have the same number of terms. -}
 instance (Num a, Eq a) => Eq (Poly a) where
     (==) (P f) (P g) 
         | length f /= length g = False
         | otherwise            = f == g
  
--- Exercise 3 -----------------------------------------
 
--- Allows polynomial types to be read as we normally read them in the REPL
--- Example: P [1,2,3] = 3x^2 + 2x + 1
--- Example: P [1,0,4] = 4x^2 + 1
--- Example: P [1,1,1] = x^2 + x + 1
+{- Allows polynomial types to be read and written 
+   as we normally read/write them in the REPL.
+   Example: P [1,2,3] = 3x^2 + 2*x + 1
+   Example: P [1,0,4] = 4x^2 + 1
+   Example: P [1,1,1] = x^2 + x + 1 -}
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show (P [a])   = show a
-    show (P f)     
-        | lead == 1   = "x^" ++ power ++ " + " ++ recurse
-        | lead == 0   = recurse 
-        | otherwise   = (show $ lead) ++ "x^" ++ power ++ " + " ++ recurse
-        where power   = show (length f - 1) --power of leading term
-              recurse = show (P (init f))   --recursive step in function
-              lead    = last f              --coefficient of leading term
+    show (P f) = showReverse $ reverse f
 
--- Exercise 4 -----------------------------------------
+{- Helper function which produces string of desired 
+   polynomial from reversed list. -}
+showReverse :: (Num a, Eq a, Show a) => [a] -> String    
+showReverse [] = error "Not a valid polynomial."
+showReverse f@(y:ys)  
+    | allZero f          = "0"
+    | y == 0             = recurse
+    | ys == [] && y /= 0 = show y
+    | y == 1 && small    = "x" ++ " + " ++ recurse
+    | small              = show y ++ "x + " ++ recurse
+    | y == 1             = "x^" ++ power ++ " + " ++ recurse
+    | otherwise          = show y ++ "x^" ++ power ++ " + " ++ recurse
+         where power     = show (length f - 1) 
+               recurse   = showReverse ys
+               small     = length ys == 1 
 
--- Allows polynomials to be added in list form
--- if one polynomial has less terms, we simply add zeroes to the end until
-    -- the lengths are equal
+{- Checks if the list is all zeroes. -}
+allZero :: (Num a, Eq a, Show a) => [a] -> Bool
+allZero f = foldl' (\acc y -> if y /= 0 then False else acc) True f
+
+
+{- Allows polynomials to be added.
+   If one polynomial, "f", has less terms than the 
+   other, we simply add terms with zeroes as 
+   coefficients to "f" until the lengths are equal. -}
 plus :: Num a => Poly a -> Poly a -> Poly a
 plus (P f) (P g) 
-    | fLen == gLen  = P (adder f g)
-    | fLen < gLen   = P (adder fWithZero g)
-    | otherwise     = P (adder f gWithZero)
-    where adder a b = zipWith (+) a b
-          fWithZero = plusHelper (gLen - fLen) f
-          gWithZero = plusHelper (fLen - gLen) g
-          fLen      = length f
-          gLen      = length g
+    | length f == length g = P (addPoly f g)
+    | length f < length g  = P (addPoly fWithZero g)
+    | otherwise            = P (addPoly f gWithZero)
+    where addPoly a b = zipWith (+) a b 
+          fWithZero = addZero (length g - length f) f
+          gWithZero = addZero (length f - length g) g
 
-plusHelper :: Num a => Int -> [a] -> [a]
-plusHelper 0 poly = poly
-plusHelper n poly = (plusHelper (n - 1) poly) ++ [0] 
+{- Helper function to add zero-coefficent terms to 
+   the polynomial with less terms. -}
+addZero :: Num a => Int -> [a] -> [a]
+addZero 0 poly = poly
+addZero n poly = (addZero (n - 1) poly) ++ [0]  
  
--- Exercise 5 -----------------------------------------
-
+{- Allows for polynomials to be multiplied.
+   If you multiply two polynomials, f and g, 
+   you take each term in f and multiply it by
+   each term in g.
+   Example: 
+   (x + 2) * (x + 4) = x*x + x*4 + 2*x + 2*4
+                     = x^2 + 6*x + 8
+   P [2,1] * P [4,1] = [2*4,2*1] + [1*0.1*4.1*1]
+                     = [8,6,1] = x^2 + 6*x + 8 -}
 times :: Num a => Poly a -> Poly a -> Poly a
 times (P []) _ = 0
-times (P (a:as)) (P f) = (P (map (*a) f)) + times (P as) (P (0:f))
+times (P (y:ys)) (P f) = (P (map (*y) f)) + times (P ys) (P (0:f))
 
--- Exercise 6 -----------------------------------------
-
+{- The 6 operations any "number" should have, 
+   as applied to polynomials. We are only 
+   defining the first four, as the absolute 
+   value of a polynomial is no longer a 
+   polynomial, and polynomials do not have a 
+   sign. -}
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
@@ -71,8 +95,8 @@ instance Num a => Num (Poly a) where
     abs    = undefined
     signum = undefined
 
--- Exercise 7 -----------------------------------------
-
+{- Takes a polynomial and a value, and evaluates 
+   the polynomial at that value. -}
 applyP :: Num a => Poly a -> a -> a
 applyP (P [])  _ = error "Not a polynomial."
 applyP (P [n]) _ = n
@@ -80,8 +104,9 @@ applyP (P f) n   = (last f) * (n ^ (power f)) + applyP (P g) n
                  where power h = length h - 1
                        g       = init f
 
--- Exercise 8 -----------------------------------------
-
+{- Defines the Diffrentiable type class, and defines the 
+   nderiv function, which takes the nth deriviative of a 
+   polynomial. -}
 class Num a => Differentiable a where
     deriv  :: a -> a
     nderiv :: Int -> a -> a
@@ -91,12 +116,13 @@ class Num a => Differentiable a where
         | n == 1    = deriv f
         | otherwise = deriv $ nderiv (n-1) f
 
--- Exercise 9 -----------------------------------------
-
+{- Takes the derivative of a polynomial using the power rule. -}
 instance Num a => Differentiable (Poly a) where
     deriv (P [])  = error "Not a valid polynomial."
-    deriv (P f)   = (P (reverse $ derivHelper f))
+    deriv (P [_]) = (P [0])
+    deriv (P f)   = (P (tail $ reverse $ derivHelper f))
 
+{- Helper function which gives the derivative in reverse order. -}
 derivHelper :: Num a => [a] -> [a]
-derivHelper [y] = [0]
+derivHelper [_] = [0]
 derivHelper f   = (genericLength f - 1) * (last f) : derivHelper (init f)
