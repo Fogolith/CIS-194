da1b3a6efb6120965e88808273be5f5eff34dc8f
ba205 <ba205@users.noreply.github.com>
2015-07-01 16:51:38 -0400

    Update mastermind.hs

--- mastermind.hs
+++ mastermind.hs
@@ -1,61 +1,74 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW02 where
+
 -- Mastermind -----------------------------------------
 
--- A peg can be one of six colors
+{- A peg can be one of six colors. -}
 data Peg = Red | Green | Blue | Yellow | Orange | Purple
          deriving (Show, Eq, Ord)
 
--- A code is defined to simply be a list of Pegs
+
+{- A code is defined to simply be a list of Pegs. -}
 type Code = [Peg]
 
--- A move is constructed using a Code and two integers; the number of
--- exact matches and the number of regular matches
+
+{- A move is constructed using a Code and two integers; the number of
+   exact matches and the number of regular matches. -}
 data Move = Move Code Int Int
           deriving (Show, Eq)
 
--- List containing all of the different Pegs
+
+{- List containing all of the different Pegs. -}
 colors :: [Peg]
 colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
--- Exercise 1 -----------------------------------------
 
--- Get the number of exact matches between the actual code and the guess
+{- Get the number of exact matches between the actual code and the guess. -}
 exactMatches :: Code -> Code -> Int
 exactMatches code1 code2 = sum $ zipWith (\x y -> if x == y then 1 else 0) code1 code2 
 
--- Exercise 2 -----------------------------------------
 
--- For each peg in xs, count how many times is occurs in ys
+{- For each peg in xs, count how many times it occurs in ys. -}
 countColors :: Code -> [Int]
---countColors code = [length $ filter (== Red) code, length $ filter (== Green) code, length $ filter (== Blue) code, length $ filter (== Yellow) code, length $ filter (== Orange) code, length $ filter (== Purple) code]
 countColors code = foldr counter [] colors
                    where counter x acc = (length $ filter (== x) code) : acc 
 
--- Count number of matches between the actual code and the guess
+
+{- Count number of matches between the actual code and the guess. -}
 matches :: Code -> Code -> Int
 matches code1 code2 = sum $ zipWith (min) (countColors code1) (countColors code2)
 
--- Exercise 3 -----------------------------------------
 
--- Construct a Move from a guess given the actual code
+{- Construct a Move from a guess given the actual code. -}
 getMove :: Code -> Code -> Move
-getMove code1 code2 = Move code2 (exactMatches code1 code2) ((matches code1 code2) - (exactMatches code1 code2))
+getMove code1 code2 = Move code2 (exactMatches') (matches' - exactMatches')
+                      where matches'      = matches code1 code2
+                            exactMatches' = exactMatches code1 code2
 
--- Exercise 4 -----------------------------------------
 
+{- Checks if a code is consistent with a given move. -}
 isConsistent :: Move -> Code -> Bool
 isConsistent move@(Move code1 _ _) code2 = (getMove code2 code1) == move
 
--- Exercise 5 -----------------------------------------
 
+{- Filters the list of all possible codes by consistency with a given move. -}
 filterCodes :: Move -> [Code] -> [Code]
 filterCodes move codes = filter (isConsistent move) codes
 
--- Exercise 6 -----------------------------------------
 
+{- Given an integer n, generates a list of all possible codes of length n. -}
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes 0 = []
+allCodes 1 = map (:[]) colors
+allCodes n = foldr newCodes	[] colors
+             where newCodes x acc = map (x:) (allCodes (n-1)) ++ acc
 
--- Exercise 7 -----------------------------------------
 
+{- Given a code of length n, return a list of Moves consistent to that code. -}
 solve :: Code -> [Move]
-solve = undefined
+solve code = filter (isConsistent' code) $ map (getMove code) $ allCodes $ length code
+
+{- isConsistent with order of inputs reversed, to take advantage of partial 
+   application in the solve function. -}
+isConsistent' :: Code -> Move -> Bool
+isConsistent' code2 move@(Move code1 _ _) = (getMove code2 code1) == move
